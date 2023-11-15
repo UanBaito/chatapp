e8c81db1bee253522345947689a428b78d610533
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-15 19:11:31 -0400

    import main components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import "./App.css";
+import ChatHistory from "./chat-history/ChatHistory";
+import ChatNewMessage from "./chat-newmessage/ChatNewMessage";
+import ChatSidebar from "./chat-sidebar/ChatSidebar";
 
 export default function App() {
   return (
     <>
+      <ChatSidebar />
+      <ChatHistory />
+      <ChatNewMessage />
     </>
-  )
+  );
 }
-
