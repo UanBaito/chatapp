ba572bf855c077f1b9fa6d28c97dff52a46214f8
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-15 19:23:35 -0400

    create useConnection hook to connect to websocket server

---
+++ src/common/hooks/useConnection.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function useConnection() {
+  const [connection, setConnection] = useState<WebSocket>();
+  useEffect(() => {
+    const webSocket = new WebSocket(
+      "wss://lgjdl0ucsf.execute-api.us-east-2.amazonaws.com/development/",
+    );
+    setConnection(webSocket);
+    return () => {
+      connection?.close();
+    };
+  });
+  return connection;
+}
