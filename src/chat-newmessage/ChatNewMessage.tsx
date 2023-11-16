4977a52a587d26ff1dd2edb2db7c0130af3bfa03
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-15 20:16:29 -0400

    add basic functionality to send message

--- src/chat-newmessage/ChatNewMessage.tsx
+++ src/chat-newmessage/ChatNewMessage.tsx
@@ -1,10 +1,29 @@
-export default function ChatNewMessage() {
+import { useState } from "react";
+
+export default function ChatNewMessage(
+  { connection }: { connection: WebSocket },
+) {
+  const [inputValue, setInputValue] = useState("");
+
+  function sendMessage(message: string) {
+    connection.send(JSON.stringify({ action: "message", message: message }));
+  }
+
   return (
     <section>
       <h2>Send message</h2>
-      <form onSubmit={(e) => {e.preventDefault()}}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage(inputValue);
+        }}
+      >
         <section>
-          <input name="message" />
+          <input
+            name="message"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </section>
       </form>
     </section>
