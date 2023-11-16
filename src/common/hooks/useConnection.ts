62c71aee238063d2788ae45131571b2785ec210d
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-16 15:47:36 -0400

    update useConnection to fix bugs related to rerendering

--- src/common/hooks/useConnection.ts
+++ src/common/hooks/useConnection.ts
@@ -1,18 +1,43 @@
 import { useEffect, useState } from "react";
+import { Message } from "../types";
 
 export default function useConnection() {
-  const [connection, setConnection] = useState<WebSocket | null>(null);
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [readyState, setReadyState] = useState<0 | 1 | 2 | 3>(WebSocket.CLOSED);
+  const [message, setMessage] = useState<Message | null>(null);
+
   useEffect(() => {
-    if (connection === null) {
-      setConnection(
-        new WebSocket(
-          "wss://lgjdl0ucsf.execute-api.us-east-2.amazonaws.com/development/",
-        ),
-      );
-    }
-    return () => {
-      connection?.close();
+    const newWebSocket = new WebSocket(
+      "wss://lgjdl0ucsf.execute-api.us-east-2.amazonaws.com/development/",
+    );
+    newWebSocket.onopen = () => {
+      console.log("Connected!");
+      setReadyState(WebSocket.OPEN);
     };
-  }, [connection, setConnection]);
-  return connection;
+    newWebSocket.onclose = () => {
+      console.log("Closed!");
+      setReadyState(WebSocket.CLOSED);
+    };
+    newWebSocket.onmessage = (event) => {
+      console.log("Received message!");
+      setMessage(JSON.parse(event.data));
+    };
+    setWs(newWebSocket);
+
+    return () => {
+      //in strict mode, react will try to close the connection before it was even established.
+      //This is to prevent that.
+      if (newWebSocket.readyState === 1) {
+        newWebSocket.onclose = null;
+        newWebSocket.onopen = null;
+        newWebSocket.onmessage = null;
+        newWebSocket.close();
+      } else {
+        newWebSocket.addEventListener("open", () => {
+          newWebSocket.close();
+        });
+      }
+    };
+  }, []);
+  return { ws, readyState, message };
 }
