bf2e7038ac80396f4b2eb9e99a69684f3c6f5784
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-15 20:15:57 -0400

    update app to use useConnection hook and pass it as prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import "./App.css";
 import ChatHistory from "./chat-history/ChatHistory";
 import ChatNewMessage from "./chat-newmessage/ChatNewMessage";
 import ChatSidebar from "./chat-sidebar/ChatSidebar";
+import useConnection from "./common/hooks/useConnection";
 
 export default function App() {
+  const connection = useConnection();
+  if (connection?.readyState) {
+    connection.onmessage = (event: MessageEvent) => {
+      console.log(event);
+    };
+  }
   return (
-    <>
-      <ChatSidebar />
-      <ChatHistory />
-      <ChatNewMessage />
-    </>
+    <main>
+      {connection
+        ? (
+          <>
+            <ChatSidebar />
+            <ChatHistory connection={connection} />
+            <ChatNewMessage connection={connection}/>
+          </>
+        )
+        : <span>loading...</span>}
+    </main>
   );
 }
