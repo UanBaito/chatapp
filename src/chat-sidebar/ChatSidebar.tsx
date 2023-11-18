030ded664f6bc5767682bb40968b1ae9a8143f45
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-18 15:10:10 -0400

    update chatsidebar to listen to userlist update messages

--- src/chat-sidebar/ChatSidebar.tsx
+++ src/chat-sidebar/ChatSidebar.tsx
@@ -1,18 +1,23 @@
-import styles from './styles/ChatSidebar.module.scss'
-export default function ChatSidebar() {
+import { useEffect, useState } from "react";
+import { Message } from "../common/types";
+import styles from "./styles/ChatSidebar.module.scss";
 export default function ChatSidebar(
   { lastMessageState }: { lastMessageState: Message | null },
 ) {
+  const [userList, setUserlist] = useState<string[]>([]);
+  useEffect(() => {
+    if (lastMessageState) {
+      if (lastMessageState.type === "names") {
+        const names = lastMessageState;
+        setUserlist(names.data);
+      }
+    }
+  });
   return (
     <div className={styles.container}>
       <h2>Users</h2>
       <ul>
-        <li>
-          Alice
-        </li>
-        <li>
-          Bob
-        </li>
+        {userList.map((name) => <li>{name}</li>)}
       </ul>
     </div>
   );
