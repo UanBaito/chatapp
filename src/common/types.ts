51f2b79f5cf464040eb301a22cd8185262150094
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-19 13:39:02 -0400

    add Connection type for useConnection hook return value

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,3 +3,11 @@ export type Message = {
   data: any;
   id: string;
 };
+export type Connection = {
+  ws: WebSocket | null;
+  readyState: 0 | 2 | 1 | 3;
+  message: Message | null;
+  userInfo: {
+    name: string;
+  };
+};
