8f445962c0b0f3465d60fedba839d7e70230b55d
UanBaito <fernandoanibalreyes@gmail.com>
2023-12-04 19:11:59 -0400

    delete unused vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      host: "dev.lan.fernandoanibal.com",
-      protocol: "wss",
-    },
-  },
 });
