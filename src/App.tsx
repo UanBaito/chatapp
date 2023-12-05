654bdc34d0382c653ef15f67df2046b6d33849a9
UanBaito <fernandoanibalreyes@gmail.com>
2023-12-04 22:30:20 -0400

    update layout on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import ChatHistory from "./chat-history/ChatHistory";
 import ChatNewMessage from "./chat-newmessage/ChatNewMessage";
 import ChatSidebar from "./chat-sidebar/ChatSidebar";
 import useConnection from "./common/hooks/useConnection";
-import "./common/styles/_global.scss"
+import "./common/styles/_global.scss";
 
 export default function App() {
   const connection = useConnection();
+  const [isSidebarOpen, setIsSideBarOpen] = useState(false);
   if (connection.ws === null) {
     return <span>Establishing connection...</span>;
   }
 
   return (
-    <main className={styles.chatbox}>
+    <main className={styles.chatbox} data-is-sidebar-open={isSidebarOpen}>
       {connection.readyState === 1
         ? (
           <>
+            <div className="mobile_nav">
+              <input
+                type="checkbox"
+                className="sidebar_close_checkbox"
+                value={isSidebarOpen}
+                onChange={() => {
+                  setIsSideBarOpen(!isSidebarOpen);
+                }}
+              />
+            </div>
             <section className={styles.chat_sidebar} aria-label="List of users">
               <ChatSidebar lastMessageState={connection.message} />
             </section>
@@ -26,7 +38,10 @@ export default function App() {
               className={styles.chat_newmessage}
               aria-label="Send new message"
             >
-              <ChatNewMessage connection={connection.ws} userInfo={connection.userInfo} />
+              <ChatNewMessage
+                connection={connection.ws}
+                userInfo={connection.userInfo}
+              />
             </section>
           </>
         )
